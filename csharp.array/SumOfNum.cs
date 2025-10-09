b7c32fc8b5d63fbe575170bbe58d793f701c0f84
Kiran97685 <kiran.kurella97685@gmail.com>
2025-10-09 16:16:56 -0500

    Commit Sum Of Numbers

---
+++ csharp.array/SumOfNum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp.arrays
+{
+    public class SumOfNum
+    {
+        public static void CalculateSum()
+        {
+            Console.Write("Enter a number (N): ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int sum = n * (n + 1) / 2;
+
+            Console.WriteLine($"\nThe sum of first {n} natural numbers is: {sum}");
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
