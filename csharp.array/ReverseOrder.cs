b8000af3aec7a16fbf2bf048f9b2c165a16834d4
Kiran97685 <kiran.kurella97685@gmail.com>
2025-10-10 18:47:56 -0500

    ReverseOrder Commit

---
+++ csharp.array/ReverseOrder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace csharp.arrays
+{
+    internal class ArrayReverse
+    {
+        public static void PrintReverse()
+        {
+            Console.Write("Enter the number of elements in the array: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            // Input elements
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Enter element {i + 1}: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nArray elements in reverse order:");
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine("\n\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
